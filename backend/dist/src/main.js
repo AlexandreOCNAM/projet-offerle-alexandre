439ec8d98aa008613bf2600adda3db39e81a6f85
Alexandre <aofferle@cfai-formation.fr>
2024-01-12 11:15:15 +0100

    ajout de helmet et changement cors

--- backend/dist/src/main.js
+++ backend/dist/src/main.js
@@ -4,6 +4,8 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const swagger_1 = require("@nestjs/swagger");
 const common_1 = require("@nestjs/common");
+const express_rate_limit_1 = require("express-rate-limit");
+const helmet_1 = require("helmet");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
@@ -18,6 +20,15 @@ async function bootstrap() {
         whitelist: true,
         transform: true,
         forbidNonWhitelisted: true,
+    }));
+    app.use((0, helmet_1.default)());
+    app.use((0, express_rate_limit_1.default)({
+        windowMs: 10 * 60 * 1000,
+        max: 100,
+        standardHeaders: true,
+        legacyHeaders: false,
+        skipSuccessfulRequests: false,
+        message: { message: 'Too many requests', statusCode: 403 },
     }));
     const config = new swagger_1.DocumentBuilder()
         .setTitle('Median')
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: 'https://projet-offerle-alexandre-front.onrender.com',
     credentials: true,
     allowedHeaders: 'Content-Type, Accept, Authorization',
     preflightContinue: false,
@@ -20,6 +22,19 @@ async function bootstrap() {
       whitelist: true,
       transform: true,
       forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.use(helmet());
+
+  app.use(
+    rateLimit({
+      windowMs: 10 * 60 * 1000, // 10 minutes
+      max: 100, // Limit each IP to 100 requests per `window` (here, per 10 minutes)
+      standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+      legacyHeaders: false, // Disable the `X-RateLimit-*` headers,
+      skipSuccessfulRequests: false, // The counting will skip all successful requests and just count the errors. Instead of removing rate-limiting, it's better to set this to true to limit the number of times a request fails. Can help prevent against brute-force attacks
+      message: { message: 'Too many requests', statusCode: 403 },
     }),
   );
 
